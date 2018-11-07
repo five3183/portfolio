c8638980af21b67b55d04de742776601e50d575c
Matthew Curran <five3183@gmail.com>
2018-11-06 22:18:48 -0500

    js update

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,39 @@
-$(function () {
-    $('[data-toggle="popover"]').popover({html:true})
+const projects = document.querySelectorAll('.project-container')
+const overlays = document.querySelectorAll('.overlay')
+
+const resumeBtn = document.getElementById('resume-btn')
+const resume = document.getElementById('resume')
+const closeBtn = document.getElementById('close-btn')
+
+projects.forEach(project => {
+  project.addEventListener('mouseover', (e) => {
+    projectDiv = e.target.parentNode.childNodes
+    projectDiv.forEach(element => {
+      // console.log(element.nodeType)
+      if(element.className === 'overlay invisible') {
+        element.className = 'overlay visible'
+      }
+    })
   })
+})
+
+overlays.forEach(overlay => {
+  overlay.addEventListener('mouseout', (e) => {
+    overlayDiv = e.target.parentNode.childNodes
+    overlayDiv.forEach(element => {
+      if(element.className === 'overlay visible') {
+        element.className = 'overlay invisible'
+      }
+    })
+  })
+})
+
+resumeBtn.addEventListener('click', () => {
+  if(resume.className === 'invisible') {
+    resume.className = 'visible'
+  }
+})
+
+closeBtn.addEventListener('click', () => {
+  resume.className = 'invisible'
+})
