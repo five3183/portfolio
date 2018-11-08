09de94c1ebde4c2393f9f6d66b6464604c3083c2
Matthew Curran <five3183@gmail.com>
2018-11-08 13:15:50 -0500

    formatting update

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,26 +5,33 @@ const resumeBtn = document.getElementById('resume-btn')
 const resume = document.getElementById('resume')
 const closeBtn = document.getElementById('close-btn')
 
+const hideOverlay = (e) => {
+  overlayDiv = e.target.parentNode.childNodes
+	overlayDiv.forEach(element => {
+		if(element.className === 'overlay visible') {
+			element.className = 'overlay invisible'
+		}
+	})
+}
+
+const showOverlay = (e) => {
+	projectDiv = e.target.parentNode.childNodes
+	projectDiv.forEach(element => {
+		if(element.className === 'overlay invisible') {
+			element.className = 'overlay visible'
+		}
+    })
+}
+
 projects.forEach(project => {
   project.addEventListener('mouseover', (e) => {
-    projectDiv = e.target.parentNode.childNodes
-    projectDiv.forEach(element => {
-      // console.log(element.nodeType)
-      if(element.className === 'overlay invisible') {
-        element.className = 'overlay visible'
-      }
-    })
+	  showOverlay(e)
   })
 })
 
 overlays.forEach(overlay => {
   overlay.addEventListener('mouseout', (e) => {
-    overlayDiv = e.target.parentNode.childNodes
-    overlayDiv.forEach(element => {
-      if(element.className === 'overlay visible') {
-        element.className = 'overlay invisible'
-      }
-    })
+	setTimeout(() => {hideOverlay(e)}, 5 * 1000)
   })
 })
 
